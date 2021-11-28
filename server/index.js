7be0017181229caa4bafdfce1e3ad01bff3d5975
Iain Furstenwerth <iain.furstenwerth@gmail.com>
2021-11-28 14:15:44 -0600

    deployed version not working.

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 const compression = require('compression');
 const cors = require('cors');
 const path = require('path');
 
+const options = {
+  key: fs.readFileSync('dist/certs/openSSLkey.pem'),
+  cert: fs.readFileSync('dist/certs/zoe-emery.com.crt')
+};
+
 const app = express();
-const port = 80;
 
 app.use(compression());
 app.use(express.static(path.join(__dirname, '../dist')));
@@ -14,6 +21,5 @@ app.get('/', (req, res) => {
   res.send();
 });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}...`);
-});
+// http.createServer(app).listen(80);
+https.createServer(options, app).listen(443);
