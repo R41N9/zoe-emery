0369056faa6d55ffd578202dc97ffb31bd825a6a
Iain Furstenwerth <iain.furstenwerth@gmail.com>
2021-11-28 01:02:45 -0600

    basic react layout complete

--- components/Releases.jsx
+++ components/Releases.jsx
@@ -8,7 +8,18 @@ class Releases extends React.Component {
 
   render() {
     return (
-
+      <div id="releases" className="content">
+        <h2 id="releases-title">ALL RELEASES</h2>
+        <a href="./music/darkBeat.mp3" download>
+          <img id="eris-eros" src="./imgs/zoe-emery-eris-eros.jpeg" alt="Eris Eros"/>
+        </a>
+        <a href="./music/darkBeat.mp3" download>
+          <img id="eris-eros-pt-2" src="./imgs/zoe-emery-eris-eros.jpeg" alt="Eris Eros, Pt. 2"/>
+        </a>
+        <a href="./music/darkBeat.mp3" download>
+          <img id="hydrochloria" src="./imgs/zoe-emery-eris-eros.jpeg" alt="Hydrochloria"/>
+        </a>
+      </div>
     )
   }
 }
--- components/Update.jsx
+++ components/Update.jsx
@@ -12,8 +12,8 @@ class Update extends React.Component {
         <img id="update-img" src="./imgs/zoe-emery-show.html" alt="Zoe Emery @ Scout Bar 11/21/2021"/>
         <p id="bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna neque viverra justo nec ultrices dui sapien eget. Vulputate ut pharetra sit amet aliquam id diam. Id cursus metus aliquam eleifend mi in nulla.</p>
         <p id="news">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna neque viverra justo nec ultrices dui sapien eget. Vulputate ut pharetra sit amet aliquam id diam. Id cursus metus aliquam eleifend mi in nulla.</p>
-        <a href="./music/darkBeat.mp3" download>
-          <img id="latest-release" src="./imgs/zoe-emery-somatic-symmetry.jpeg" alt="Somatic Symmetry, Out of My System, Etcetera"/>
+        <a href="./music/darkBeat.mp3" id="latest-release" className="download" download>
+          <img id="latest-release-img" src="./imgs/zoe-emery-somatic-symmetry.jpeg" alt="Somatic Symmetry, Out of My System, Etcetera"/>
           <span id="latest-release-indicator">Out Now...</span>
         </a>
       </div>
