c2bd51dd754c4ca2e6c184a2cfdf6b467a1f39f2
Iain Furstenwerth <iain.furstenwerth@gmail.com>
2021-11-28 11:52:23 -0600

    website complete except for real info

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,6 +29,11 @@ class Header extends React.Component {
         </div>
         <img id="top-right-flourish-header" className="top-right flourish" src="./imgs/red-corner-zoe-emery.png" alt="flourish"/>
         <h1 id="title" className="header-item item">Zo&euml; Emery</h1>
+        <div id="links" className="box">
+          <a id="insta-link" className="link" href="https://www.instagram.com/zoe.emery.music/?hl=en"><img id="insta-link-img" className="link-img" src="./imgs/instagram-logo.png" alt="insta logo"/></a>
+          <a id="twitter-link" className="link" href="https://twitter.com/dysdaimonia"><img id="twitter-link-img" className="link-img" src="./imgs/twitter-logo.png" alt="twitter logo"/></a>
+          <a id="fb-link" className="link" href="https://www.facebook.com/ZoeEmeryMusic"><img id="fb-link-img" className="link-img" src="./imgs/facebook-logo.png" alt="facebook logo"/></a>
+        </div>
         <img id="bottom-left-flourish-header" className="bottom-left flourish" src="./imgs/red-corner-zoe-emery.png" alt="flourish"/>
       </div>
     )
